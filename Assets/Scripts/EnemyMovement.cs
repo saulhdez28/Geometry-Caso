09eaef097fbaec9001850601afd9bdae938156fe
Saul Hernandez <saulhdez28@gmail.com>
2024-08-25 23:00:36 -0600

    Enemy Attack animation
    Enemy Attack animation

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Threading.Tasks;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,215 +10,235 @@ namespace StarterAssets
     [RequireComponent(typeof(NavMeshAgent))]
     public class EnemyMovement : MonoBehaviour
     {
-        [Header("Player")]
-        [Tooltip("Move speed of the character in m/s")]
-        public float MoveSpeed = 2.0f;
+        public enum EnemyState
+        {
+            Idle,
+            Walking,
+            Running,
+            Attacking
+        }
 
-        [Tooltip("Sprint speed of the character in m/s")]
-        public float SprintSpeed = 5.335f;
+        public enum AttackMode
+        {
+            None = 0,
+            Jab = 1,
+            KO = 2,
+            HighKick = 3,
+            LowKick = 4,
+            Uppercut = 5
+        }
 
-        [Tooltip("How fast the character turns to face movement direction")]
-        [Range(0.0f, 0.3f)]
-        public float RotationSmoothTime = 0.12f;
+        [Header("Enemy")]
 
-        [Tooltip("Acceleration and deceleration")]
-        public float SpeedChangeRate = 10.0f;
+        [SerializeField]
+        private float MoveSpeed = 2.0f;
 
-        [Tooltip("Stopping distance to reach the target")]
-        public float StoppingDistance = 0.5f;
+        [SerializeField]
+        private float SprintSpeed = 5.335f;
 
-        public AudioClip LandingAudioClip;
-        public AudioClip[] FootstepAudioClips;
-        [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
+        [SerializeField]
+        private float RotationSmoothTime = 0.12f;
 
-        [Space(10)]
-        [Tooltip("The height the player can jump")]
-        public float JumpHeight = 1.2f;
+        [SerializeField]
+        private float _rotationVelocity = 0.1f;
 
-        [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-        public float Gravity = -15.0f;
+        [SerializeField]
+        private float SpeedChangeRate = 10.0f;
 
-        [Space(10)]
-        [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
-        public float JumpTimeout = 0.50f;
+        [SerializeField]
+        private float AttackRange = 2.0f;
 
-        [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
-        public float FallTimeout = 0.15f;
+        [SerializeField]
+        private float AttackDuration = 1.0f;
 
-        [Header("Player Grounded")]
-        [Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
-        public bool Grounded = true;
+        [SerializeField]
+        private Transform PlayerTransform;
 
-        [Tooltip("Useful for rough ground")]
-        public float GroundedOffset = -0.14f;
+        [SerializeField]
+        private float minDistance = 2.0f;
 
-        [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-        public float GroundedRadius = 0.28f;
+        [SerializeField]
+        private AudioClip LandingAudioClip;
 
-        [Tooltip("What layers the character uses as ground")]
-        public LayerMask GroundLayers;
+        [SerializeField]
+        private AudioClip[] FootstepAudioClips;
 
-        // Player
+        [SerializeField]
+        [Range(0, 1)]
+        private float FootstepAudioVolume = 0.2f;
+
+        [SerializeField]
+        private float GroundedRadius = 0.28f;
+
+        [SerializeField]
+        private LayerMask GroundLayers;
+
+        [SerializeField]
+        private float GroundedOffset = -0.14f;
+
         private float _speed;
-        private float _animationBlend;
-        private float _targetRotation = 0.0f;
-        private float _rotationVelocity;
-        private float _verticalVelocity;
-        private float _terminalVelocity = 53.0f;
-
-        // Timeout delta time
-        private float _jumpTimeoutDelta;
-        private float _fallTimeoutDelta;
-
-        // Animation IDs
         private int _animIDSpeed;
         private int _animIDGrounded;
-        private int _animIDJump;
-        private int _animIDFreeFall;
         private int _animIDMotionSpeed;
+        private int _animIDAttack;
 
         private Animator _animator;
         private CharacterController _controller;
         private NavMeshAgent _navMeshAgent;
 
-        private const float _threshold = 0.01f;
-
-        private bool _hasAnimator;
+        private EnemyState _currentState = EnemyState.Idle;
+        private AttackMode _currentAttackMode = AttackMode.None;
 
         private void Start()
         {
-            _hasAnimator = TryGetComponent(out _animator);
+            _animator = GetComponent<Animator>();
             _controller = GetComponent<CharacterController>();
             _navMeshAgent = GetComponent<NavMeshAgent>();
 
             AssignAnimationIDs();
-
-            // Reset our timeouts on start
-            _jumpTimeoutDelta = JumpTimeout;
-            _fallTimeoutDelta = FallTimeout;
-
-            // Make sure the NavMeshAgent doesn't handle rotation
             _navMeshAgent.updateRotation = false;
         }
 
         private void Update()
         {
-            _hasAnimator = TryGetComponent(out _animator);
-
-            JumpAndGravity();
             GroundedCheck();
-            Move();
+            HandleState();
         }
 
         private void AssignAnimationIDs()
         {
             _animIDSpeed = Animator.StringToHash("Speed");
             _animIDGrounded = Animator.StringToHash("Grounded");
-            _animIDJump = Animator.StringToHash("Jump");
-            _animIDFreeFall = Animator.StringToHash("FreeFall");
             _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+            _animIDAttack = Animator.StringToHash("Attack");
         }
 
         private void GroundedCheck()
         {
-            // Set sphere position, with offset
-            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
-            Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset,
+                transform.position.z);
+            bool ground = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers,
+                QueryTriggerInteraction.Ignore);
 
-            // Update animator if using character
-            if (_hasAnimator)
+            _animator.SetBool(_animIDGrounded, ground);
+        }
+
+        private void HandleState()
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, PlayerTransform.position);
+
+            switch (_currentState)
             {
-                _animator.SetBool(_animIDGrounded, Grounded);
+                case EnemyState.Idle:
+                    if (distanceToPlayer <= AttackRange && CanAttack())
+                    {
+                        _currentState = EnemyState.Attacking;
+                        StartCoroutine(AttackRoutine());
+                    }
+                    else if (distanceToPlayer > AttackRange && distanceToPlayer <= _navMeshAgent.stoppingDistance)
+                    {
+                        _currentState = EnemyState.Walking;
+                    }
+                    else if (distanceToPlayer > _navMeshAgent.stoppingDistance)
+                    {
+                        _currentState = EnemyState.Running;
+                    }
+                    break;
+
+                case EnemyState.Walking:
+                    Move(MoveSpeed);
+                    if (distanceToPlayer <= AttackRange && CanAttack())
+                    {
+                        _currentState = EnemyState.Attacking;
+                        StartCoroutine(AttackRoutine());
+                    }
+                    else if (distanceToPlayer > _navMeshAgent.stoppingDistance)
+                    {
+                        _currentState = EnemyState.Running;
+                    }
+                    break;
+
+                case EnemyState.Running:
+                    Move(SprintSpeed);
+                    if (distanceToPlayer <= AttackRange && CanAttack())
+                    {
+                        _currentState = EnemyState.Attacking;
+                        StartCoroutine(AttackRoutine());
+                    }
+                    else if (distanceToPlayer <= _navMeshAgent.stoppingDistance)
+                    {
+                        _currentState = EnemyState.Walking;
+                    }
+                    break;
+
+                case EnemyState.Attacking:
+                    // Aquí no necesitamos hacer nada directamente, ya que la corutina se encarga de esto
+                    break;
             }
         }
 
-        private void Move()
+        private IEnumerator AttackRoutine()
         {
-            float desiredSpeed = Mathf.Clamp(_navMeshAgent.velocity.magnitude, MoveSpeed, SprintSpeed);
+            SetAttackMode();
 
-            // Comprobar si el enemigo ha llegado al stopping distance
-            if (_navMeshAgent.remainingDistance <= StoppingDistance)
+            _animator.SetTrigger(_animIDAttack);
+
+            yield return new WaitForSeconds(AttackDuration);
+
+
+            _animator.ResetTrigger(_animIDAttack);
+            yield return new WaitForSeconds(1);
+            _animator.SetInteger("Mode", 0);
+            _currentState = EnemyState.Idle;
+
+        }
+
+        private void SetAttackMode()
+        {
+            _currentAttackMode = (AttackMode)UnityEngine.Random.Range(1, 6);
+
+            if (_currentAttackMode == AttackMode.KO)
+            {
+                _currentAttackMode = AttackMode.Jab;
+            }
+
+            _animator.SetInteger("Mode", (int)_currentAttackMode);
+        }
+
+        private bool CanAttack()
+        {
+            return _speed < 0.01f && _animator.GetBool(_animIDGrounded);
+        }
+
+        private void Move(float speed)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, PlayerTransform.position);
+
+            if (distanceToPlayer <= minDistance)
+            {
+                _speed = Mathf.Lerp(_speed, 0f, SpeedChangeRate * Time.deltaTime / minDistance);
+            }
+            else
+            {
+                _speed = Mathf.MoveTowards(_speed, speed, SpeedChangeRate * Time.deltaTime);
+
+            }
+
+            if (_speed < 0.01f)
             {
                 _speed = 0f;
             }
-            else
-            {
-                // Acelerar o desacelerar hacia la velocidad deseada
-                _speed = Mathf.MoveTowards(_speed, desiredSpeed, SpeedChangeRate * Time.deltaTime);
-            }
 
-            // Actualizar la animación con la velocidad actual
-            _animationBlend = Mathf.Lerp(_animationBlend, _speed, Time.deltaTime * SpeedChangeRate);
-            if (_animationBlend < 0.01f) _animationBlend = 0f;
+            _navMeshAgent.SetDestination(PlayerTransform.position);
 
-            // Rotar el enemigo hacia la dirección del movimiento
             if (_navMeshAgent.velocity.sqrMagnitude > 0.01f)
             {
-                _targetRotation = Mathf.Atan2(_navMeshAgent.velocity.x, _navMeshAgent.velocity.z) * Mathf.Rad2Deg;
-                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, RotationSmoothTime);
-
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, Mathf.Atan2(_navMeshAgent.velocity.x, _navMeshAgent.velocity.z) * Mathf.Rad2Deg, ref _rotationVelocity, RotationSmoothTime);
                 transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
             }
 
-            // Actualizar el animador si se está usando uno
-            if (_hasAnimator)
-            {
-                _animator.SetFloat(_animIDSpeed, _animationBlend);
-                _animator.SetFloat(_animIDMotionSpeed, _speed);
-            }
-        }
-
-        private void JumpAndGravity()
-        {
-            if (Grounded)
-            {
-                _fallTimeoutDelta = FallTimeout;
-
-                if (_hasAnimator)
-                {
-                    _animator.SetBool(_animIDJump, false);
-                    _animator.SetBool(_animIDFreeFall, false);
-                }
-
-                if (_verticalVelocity < 0.0f)
-                {
-                    _verticalVelocity = -2f;
-                }
-            }
-            else
-            {
-                _jumpTimeoutDelta = JumpTimeout;
-
-                if (_fallTimeoutDelta >= 0.0f)
-                {
-                    _fallTimeoutDelta -= Time.deltaTime;
-                }
-                else
-                {
-                    if (_hasAnimator)
-                    {
-                        _animator.SetBool(_animIDFreeFall, true);
-                    }
-                }
-            }
-
-            if (_verticalVelocity < _terminalVelocity)
-            {
-                _verticalVelocity += Gravity * Time.deltaTime;
-            }
-        }
-
-        private void OnDrawGizmosSelected()
-        {
-            Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
-            Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
-
-            if (Grounded) Gizmos.color = transparentGreen;
-            else Gizmos.color = transparentRed;
-
-            Gizmos.DrawSphere(
-                new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z),
-                GroundedRadius);
+            _animator.SetFloat(_animIDSpeed, _speed);
+            _animator.SetFloat(_animIDMotionSpeed, 1);
         }
 
         private void OnFootstep(AnimationEvent animationEvent)
@@ -224,7 +247,7 @@ namespace StarterAssets
             {
                 if (FootstepAudioClips.Length > 0)
                 {
-                    var index = Random.Range(0, FootstepAudioClips.Length);
+                    var index = UnityEngine.Random.Range(0, FootstepAudioClips.Length);
                     AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_controller.center), FootstepAudioVolume);
                 }
             }
